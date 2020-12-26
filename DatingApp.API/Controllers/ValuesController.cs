95005a3151fdabaacc092591925a5961a8f0d7de
gokselturk <gokselturk@hotmail.com>
2020-12-26 19:22:17 +0300

    git pull --rebase origin develop

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context=context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string[]>> GetValues()
+        {
+           var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetValue(int id)
+        {
+           var value = _context.Values.FirstOrDefault(x=>x.Id == id);
+            return Ok(value);
+        }
+    }
+}
